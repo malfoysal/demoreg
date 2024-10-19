7811f149a23ba00aff795eb059d2efb38935fadf
Md Foysal Mal <107479386+malfoysal@users.noreply.github.com>
2024-10-19 12:53:34 +0600

    Update Program.cs
    Extent report

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using OpenQA.Selenium;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 public class demoregistration
@@ -7,8 +9,22 @@ public class demoregistration
     static void Main()
     {
         IWebDriver driver = new ChromeDriver();
+
+        ExtentReports extentReports = new ExtentReports();
+
+        ExtentSparkReporter reportpath = new ExtentSparkReporter(@"D:\ReportLocation\Report.html");
+        extentReports.AttachReporter(reportpath);
+
+        ExtentTest test = extentReports.CreateTest("login test", "this is our first test case");
+
+
         driver.Navigate().GoToUrl("https://parabank.parasoft.com/parabank/index.htm");
+
+        test.Log(Status.Info, "open browser");
+
         Console.WriteLine("Open browser");
+
+
         driver.Manage().Window.Maximize();
         IWebElement loginButton = driver.FindElement(By.CssSelector("#loginPanel > form > div:nth-child(5) > input"));
         if (loginButton.Displayed)
@@ -23,5 +39,8 @@ public class demoregistration
 
         driver.FindElement(By.CssSelector("#customer\\.firstName")).SendKeys("don");
         driver.FindElement(By.CssSelector("#customer\\.lastName")).SendKeys("jee");
+
+        driver.Quit();
+        extentReports.Flush();
     }
 }
